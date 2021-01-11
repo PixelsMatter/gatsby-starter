d2cb8d93e8b765e2a9bdf59659651a56db4882dd
Leon Burman シ <pixelsmatter1@gmail.com>
2021-01-11 13:05:44 +0000

    Update indentation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
+
 /**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+* Implement Gatsby's Node APIs in this file.
+*
+* See: https://www.gatsbyjs.com/docs/node-apis/
+*/
 
 // You can delete this file if you're not using it
