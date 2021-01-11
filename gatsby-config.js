ca1d5684130bb3104205b1ca46606eb6fd448bfd
Leon Burman シ <leon@pixelsmatter.io>
2021-01-11 16:30:07 +0000

    Add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
 		},
 		`gatsby-plugin-sass`,
 		`gatsby-plugin-sitemap`,
+		`gatsby-plugin-offline`,
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
