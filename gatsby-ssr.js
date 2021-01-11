9d75352acd29b0382906c2f90f21fda521fed7d7
Leon Burman シ <leon@pixelsmatter.io>
2021-01-11 13:05:44 +0000

    Update indentation

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,8 @@
+
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+* Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+*
+* See: https://www.gatsbyjs.com/docs/ssr-apis/
+*/
 
 // You can delete this file if you're not using it
