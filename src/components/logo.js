36919b55464f4d0f81fa40c82491c1d11ac1b9f0
Leon Burman シ <leon@pixelsmatter.io>
2021-01-11 13:38:56 +0000

    Add logo alt text

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 import logoPath from '../images/pixelsmatter-logo.svg'
 
 const Logo = () => (
-	<img src={logoPath} />
+	<img src={logoPath} alt="PixelsMatter logo" />
 )
 
 export default Logo
