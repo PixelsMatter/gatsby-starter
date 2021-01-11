ca1b6478eb3e11d5d70b0658e460ff5dfd3e8394
Leon Burman シ <pixelsmatter1@gmail.com>
2021-01-11 13:37:15 +0000

    Add logo component

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Logo from '../components/logo'
 
 const Header = ({ siteTitle }) => (
 	<header
@@ -17,17 +18,9 @@ const Header = ({ siteTitle }) => (
 				padding: `1.45rem 1.0875rem`,
 			}}
 		>
-			<h1 style={{ margin: 0 }}>
-				<Link
-					to="/"
-					style={{
-						color: `white`,
-						textDecoration: `none`,
-					}}
-				>
-					{siteTitle}
-				</Link>
-			</h1>
+		<Link>
+			<Logo />
+		</Link>
 		</div>
 	</header>
 )
---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import logoPath from '../images/pixelsmatter-logo.svg'
+
+const Logo = () => (
+	<img src={logoPath} />
+)
+
+export default Logo
