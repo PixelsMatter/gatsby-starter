4528222ac97e6e4c008036e1dfe62649e3ffd4f3
Leon Burman シ <pixelsmatter1@gmail.com>
2021-01-31 13:30:33 +0000

    Style buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
 			<Logo />
 			<p>This is a fresh instance of the PixelsMatter Gatsby starter which includes Preact, SCSS using a variant of 7-1 architecture, BEM, perfect Lighthouse scores, offline support, Google Analytics, sitemaps &amp; plenty more good stuff!</p>
 			<a className="btn" href="https://github.com/PixelsMatter/gatsby-template" target="_blank" rel="noreferrer">GitHub Repo</a>
-			<a className="btn" href="https://pixelsMatter.io" target="_blank" rel="noreferrer">Website</a>
+			<a className="btn btn--alt" href="https://pixelsMatter.io" target="_blank" rel="noreferrer">Website</a>
 		</Container>
 	</Layout>
 )
