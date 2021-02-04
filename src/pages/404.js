49df4ceefaf44b0ad44c60704f89c3ee423bbb4c
Leon Burman シ <leon@pixelsmatter.io>
2021-02-04 14:59:36 +0000

    Don't open home page in new tab

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
 		<Container>
 			<h1>Nothing To See Here</h1>
 			<p>Sorry, we can't seem to find the page you're looking for.</p><p>Click the button below and we'll take you back to the home page.</p>
-			<Link className="btn" to="/" target="_blank" rel="noreferrer">Home</Link>
+			<Link className="btn" to="/">Home</Link>
 		</Container>
 	</Layout>
 )
