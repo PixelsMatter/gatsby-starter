4cfcd1fa6837e83bc09935de982f930a7fde309d
Leon Burman シ <pixelsmatter1@gmail.com>
2021-03-11 18:44:01 +0000

    Clear cached files

--- src/components/CookieConsent.js
+++ src/components/CookieConsent.js
@@ -6,7 +6,7 @@ import Container from './Container'
 class CookieConsent extends Component {
 	render() {
 		return (
-			<div className={classNames('cookie-consent', {'cookie-consent--accepted': this.state.cookiesAccepted})}>
+			<div className={classNames('cookie-consent', {'cookie-consent--accepted': ''})}>
 				<Container>
 					<p className="cookie-consent__text">We use cookies to help deliver the best experience possible.</p>
 					<button className="cookie-consent__accept">Accept</button>
