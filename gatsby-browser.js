17db7366d3c4782daf12e5f1f9ae0baccd0e64dc
Leon Burman シ <pixelsmatter1@gmail.com>
2021-04-10 12:00:34 +0100

    Leave calling card in console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -18,3 +18,20 @@ import App from './src/components/App'
 export const wrapRootElement = ({ element }) => (
 	<App>{element}</App>
 )
+
+// Leave calling card in console
+console.log(`
+
+______ _          _     ___  ___      _   _            
+| ___ (_)        | |    |  \\/  |     | | | |           
+| |_/ /___  _____| |___ | .  . | __ _| |_| |_ ___ _ __ 
+|  __/| \\ \\/ / _ \\ / __|| |\\/| |/ _\` | __| __/ _ \\ '__|
+| |   | |>  <  __/ \\__ \\| |  | | (_| | |_| ||  __/ |   
+\\_|   |_/_/\\_\\___|_|___/\\_|  |_/\\__,_|\\__|\\__\\___|_|   
+
+Digitise today.
+
+Like our code? Get in touch!
+hello@pixelsmatter.io                                                       
+                                                       
+`);
