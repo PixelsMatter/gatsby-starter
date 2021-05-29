7e21a98fc2eb199b20481ea9f0a0fc02a1460046
Leon Burman シ <pixelsmatter1@gmail.com>
2021-05-29 17:43:32 +0100

    Add Lighthouse CI

---
+++ .lighthouserc.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+	ci: {
+		assert: {
+			assertions: {
+				'categories:performance': ['error', {minScore: 0.9}],
+				'categories:accessibility': ['error', {minScore: 0.9}],
+				'categories:best-practices': ['error', {minScore: 0.9}],
+				'categories:seo': ['error', {minScore: 0.9}],
+			},
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
