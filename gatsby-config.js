bed4dffe4650efc99a173567ffc0d6afc0bc2af4
Leon Burman シ <leon@pixelsmatter.io>
2021-05-31 12:27:16 +0100

    Disable offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-sitemap`,
-		`gatsby-plugin-offline`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
